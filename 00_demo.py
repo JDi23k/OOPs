18af07c0ba6b83901f156050c805b293db4d3e86
JDi23k <158051329+JDi23k@users.noreply.github.com>
2024-02-21 18:22:15 +0530

    Add files via upload

---
+++ 00_demo.py
@@ -0,0 +1,14 @@
+class Computer:
+
+
+    # Attributes --> Variables
+    # Behaviour --> Methods(Function)
+
+    def config(self):
+        print("i5, 16GB, 1TB")
+
+
+com1 = Computer()
+
+print(type(com1))
+print(com1.config())
